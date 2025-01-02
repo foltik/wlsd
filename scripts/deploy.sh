9920a5e3e8ee171edc64d5ae1ad2fb5d05a52635
Jack Foltz <jack@foltz.io>
2025-01-02 17:49:50 -0500

    Use release instead of prod profile

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,11 +5,11 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-cargo build --profile prod --target aarch64-unknown-linux-gnu
+cargo build --release --target aarch64-unknown-linux-gnu
 
 ls -l target/aarch64-unknown-linux-gnu/
 ls -l target/aarch64-unknown-linux-gnu/*
-rsync --rsync-path="sudo rsync" -Pavzr --delete assets templates config target/aarch64-unknown-linux-gnu/prod/lsd $1:/home/lsd/
+rsync --rsync-path="sudo rsync" -Pavzr --delete assets templates config target/aarch64-unknown-linux-gnu/release/lsd $1:/home/lsd/
 ssh $1 <<'EOS'
 sudo setcap 'cap_net_bind_service=+ep' /home/lsd/lsd
 sudo systemctl restart lsd
